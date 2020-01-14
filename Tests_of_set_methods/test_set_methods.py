4547ac89fbd5d9160744f2f199babfbeb0617153
Ivan_Chistov <Darksol89@gmail.com>
2020-01-14 21:48:51 +0300

    Добавлены новые тесты на проверку методов множества

--- Tests_of_set_methods/test_set_methods.py
+++ Tests_of_set_methods/test_set_methods.py
@@ -17,3 +17,25 @@ def test_adding_element_in_set(create_set, set_element):
 def test_remove_element_from_set(create_set, set_element):
     create_set.remove(set_element)
     assert len(create_set) > 8
+
+
+# Тест на объединение двух множеств
+def test_update_set(create_set):
+    new_set = set()
+    new_set.update(create_set)
+
+    assert new_set == create_set
+
+
+# Тест на удаление всех элементов из множества
+def test_clear_all_set(create_set):
+    create_set.clear()
+    assert len(create_set) != 10
+
+
+# Тест на сравнение 2-х сетов, дефолтного и с измененными значениями элементов
+@pytest.mark.parametrize('new_element', [2, 8, 14])
+def test_compare_sets(create_set, new_element):
+    new_set = create_set.copy()
+    create_set.remove(new_element)
+    assert new_set == create_set
