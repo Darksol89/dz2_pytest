9982a7ed79a40ceb821ea93845a57e727432f6c9
ichistov <ichistov@eng.vsofts.com>
2020-01-15 10:25:44 +0300

    Добавлены новые тесты на методы словаря

--- Test_of_dictionary_methods/test_dictionary_methods.py
+++ Test_of_dictionary_methods/test_dictionary_methods.py
@@ -4,14 +4,35 @@
 
 import pytest
 
+
 # Тест на удаление значений из словаря
-@pytest.mark.parametrize('value', [4, 2, 10])
+@pytest.mark.parametrize('value', [4, 2, 7])
 def test_remove_element_from_dict(create_dictionary, value):
     create_dictionary.pop(value)
-
     assert not value in create_dictionary
 
-@pytest.mark.parametrize('key', 'value', [(1), (2)])
-def test_check_element_in_dict(create_dictionary, key, value):
-    create_dictionary.items(key, value)
-    assert value in create_dictionary
+
+# Тест на возвращение ключа в словаре
+@pytest.mark.parametrize('value', [0, 1, 2])
+def test_check_key_in_dictionary(create_dictionary, value):
+    assert create_dictionary.get(value) in create_dictionary
+
+
+# Тест на сравнение 2-х словарей. Пустой и не пустой
+def test_clear_dictionary(create_dictionary):
+    new_dict = create_dictionary.copy()
+    create_dictionary.clear()
+    assert new_dict != create_dictionary
+
+
+# Тест на добавление ключа-значения в словарь. И сравнение 2-х словарей
+def test_modified_dictionary(create_dictionary):
+    old_dict = create_dictionary.copy()
+    create_dictionary.setdefault(20, 0)
+    assert old_dict != create_dictionary
+
+
+# Тест на проверку наличия ключей в словаре
+@pytest.mark.parametrize('keys', [0, 1, 2])
+def test_return_keys_in_dictionary(create_dictionary, keys):
+    assert keys in create_dictionary.keys()
