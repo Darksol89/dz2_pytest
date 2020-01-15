94926f4869c17ad2998d976c021338a3a91158d2
ichistov <ichistov@eng.vsofts.com>
2020-01-15 15:38:55 +0300

    Удалены ненужные файлы

--- Test_of_dictionary_methods/conftest.py
+++
@@ -1,9 +0,0 @@
-"""
-
-"""
-import pytest
-
-@pytest.fixture()
-def create_dictionary():
-    my_dict = {a: a ** 2 for a in range(8)}
-    return my_dict
--- Tests_of_set_methods/conftest.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-@pytest.fixture()
-def create_set():
-    my_set = {i * 2 for i in range(10)}
-    return my_set
